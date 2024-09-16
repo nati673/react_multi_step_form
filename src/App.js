47f1eafab11ae6c37996ccbd73f573423d51d019
Natnael Hailu <natnaelhailu245@gmail.com>
2024-09-16 01:52:28 -0500

    project installed

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-8xl text-2sm font-bold underline hover:text-green-500">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
